91522456369dbc6dccacf3e1920abe025eeba15d
Toon Leijtens <toon.leijtens@ing.nl>
2017-02-18 17:23:23 +0100

    node keys were fetched from the wrong location

--- artifacts/entrypoint.sh
+++ artifacts/entrypoint.sh
@@ -5,12 +5,12 @@ HELP="This is a help page. \
 Available modes are: cashcow ethbox dashboard dashboardclient help."
 case $1 in
 	cashcow)
-	cp /root/key.cashcow /root/.ethereum/nodekey
+	cp /root/.ethereum/key.cashcow /root/.ethereum/nodekey
 	sed -i "s/__subnet__/$SUBNET/g" /root/.ethereum/static-nodes.json
 	/usr/local/sbin/geth --rpccorsdomain "*" --rpcapi admin,debug,shh,txpool,miner,personal,db,eth,net,web3 --identity $1 --rpcaddr $SUBNET.1 --mine --autodag --minerthreads "1" $OPTIONS
 	;;
         ethbox)
-	cp /root/key.ethbox /root/.ethereum/nodekey
+	cp /root/.ethereum/key.ethbox /root/.ethereum/nodekey
 	sed -i "s/__subnet__/$SUBNET/g" /root/.ethereum/static-nodes.json
 	/usr/local/sbin/geth --rpccorsdomain "*" --rpcapi eth,net,web3,debug --identity $1 --rpcaddr $SUBNET.2 $OPTIONS
 	;;
