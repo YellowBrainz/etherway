a14f1bb1203e8725654e5bac97ef5d765021b350
Toon Leijtens <Toon.Leijtens@ing.com>
2018-04-19 22:00:42 +0200

    missed a few cashcows on my way

--- artifacts/entrypoint.sh
+++ artifacts/entrypoint.sh
@@ -2,15 +2,15 @@
 
 OPTIONS="--nodiscover --networkid $NETWORKID --port $NETWORKPORT --rpc --rpcport $RPCPORT --etherbase adminetherbase --verbosity 6"
 HELP="This is a help page. \
-Available modes are: cashcow ethbox dashboard dashboardclient help."
+Available modes are: eth_one eth_two dashboard dashboardclient help."
 case $1 in
-	cashcow)
-	cp /root/.ethereum/key.cashcow /root/.ethereum/nodekey
+	eth_one)
+	cp /root/.ethereum/key.eth_one /root/.ethereum/nodekey
 	sed -i "s/__subnet__/$SUBNET/g" /root/.ethereum/static-nodes.json
 	/usr/local/sbin/geth --rpccorsdomain "*" --rpcapi admin,debug,shh,txpool,miner,personal,db,eth,net,web3 --identity $1 --rpcaddr $SUBNET.1 --mine --minerthreads "1" $OPTIONS
 	;;
-        ethbox)
-	cp /root/.ethereum/key.ethbox /root/.ethereum/nodekey
+        eth_two)
+	cp /root/.ethereum/key.eth_two /root/.ethereum/nodekey
 	sed -i "s/__subnet__/$SUBNET/g" /root/.ethereum/static-nodes.json
 	/usr/local/sbin/geth --rpccorsdomain "*" --rpcapi eth,net,web3,debug --identity $1 --rpcaddr $SUBNET.2 $OPTIONS
 	;;
